004ba3dee6c8ae9c1d9d95bad6f124f71fd0a4ef
Vitor Quezada <vitorquezada@gmail.com>
2019-05-27 21:40:16 -0300

    Adição dos módulos.

--- detectorDeCancer.v
+++ detectorDeCancer.v
@@ -1,5 +1,50 @@
-module detectorDeCancer(IO, reset, HEX5, HEX3, HEX2, HEX1, HEX0);
+`include "tecladoNumerico.v"
+`include "bcd7seg.v"
 
+module detectorDeCancer(IO, HEX5, HEX3, HEX2, HEX1, HEX0);
+
+	input [0:11] IO;
+	output [0:6] HEX5, HEX3, HEX2, HEX1, HEX0;
 	
-    
+	reg real [0:6] entradas;
+	reg [0] resultado;
+	reg int estado;
+	
+	parameter Pregnancies = 0,
+			  Glucose = 1,
+			  BloodPressure = 2,
+			  SkinThickness = 3,
+			  Insulin = 4,
+			  BMI = 5,
+			  Age = 6,
+			  Outcome = 7;
+
+	always @(posedge IO)
+		if(IO[11]) begin
+			estado = 0
+			entradas[0] = 0;
+			entradas[1] = 0;
+			entradas[2] = 0;
+			entradas[3] = 0;
+			entradas[4] = 0;
+			entradas[5] = 0;
+			entradas[6] = 0;
+		end
+		else if(IO[10])
+		begin
+			estado = estado + 1;			
+			if(estado == Outcome)
+			begin
+				// Chama a função que calcula.
+				
+			end
+		end
+		else			
+		begin
+			tecladoNumerico tecladoNumerico(IO[0:9], IO[10], entradas[estado], HEX3, HEX2, HEX1, HEX0);			
+		end
+		
+		bcd7seg bcd7seg(estado, HEX5);
+	end
+	
 endmodule
--- tecladoNumerico.v
+++ tecladoNumerico.v
@@ -1,4 +1,6 @@
-module tecladoNumerico(IO, numero, reset, HEX3, HEX2, HEX1, HEX0);
+`include "bcd7seg.v"
+
+module tecladoNumerico(IO, reset, numero, HEX3, HEX2, HEX1, HEX0);
 	
 	input [0:9] IO;
 	input reset; 
@@ -9,10 +11,7 @@ module tecladoNumerico(IO, numero, reset, HEX3, HEX2, HEX1, HEX0);
 	reg [3:0] [0:3] vetorNumero;
 	reg [1:0] posicao;
 	
-	wire clk;
-	assign clk = IO[0] | IO[1] | IO[2] | IO[3] | IO[4] | IO[5] | IO[6] | IO[7] | IO[8] | IO[9];
-
-	always @(posedge clk, negedge reset)
+	always @(posedge IO or posedge reset)
 		if(reset)
 		begin
 			numero = 0;
@@ -69,45 +68,10 @@ module tecladoNumerico(IO, numero, reset, HEX3, HEX2, HEX1, HEX0);
 			numero = vetorNumero[0] + vetorNumero[1] * 10 + vetorNumero[2]*100 + vetorNumero[3] * 1000;
 		end
 		
-		bcd7seg digit3 (vetorNumero[3], HEX3);
-		bcd7seg digit2 (vetorNumero[2], HEX2);
-		bcd7seg digit1 (vetorNumero[1], HEX1);
-		bcd7seg digit0 (vetorNumero[0], HEX0);
+		bcd7seg digit3(vetorNumero[3], HEX3);
+		bcd7seg digit2(vetorNumero[2], HEX2);
+		bcd7seg digit1(vetorNumero[1], HEX1);
+		bcd7seg digit0(vetorNumero[0], HEX0);
 	end
 
-endmodule
-
-module bcd7seg (bcd, display);
-	input [3:0] bcd;
-	output [0:6] display;
-
-	reg [0:6] display;
-
-	/*
-	 *       0  
-	 *      ---  
-	 *     |   |
-	 *    5|   |1
-	 *     | 6 |
-	 *      ---  
-	 *     |   |
-	 *    4|   |2
-	 *     |   |
-	 *      ---  
-	 *       3  
-	 */
-	always @ (bcd)
-		case (bcd)
-			4'h0: display = 7'b0000001;
-			4'h1: display = 7'b1001111;
-			4'h2: display = 7'b0010010;
-			4'h3: display = 7'b0000110;
-			4'h4: display = 7'b1001100;
-			4'h5: display = 7'b0100100;
-			4'h6: display = 7'b1100000;
-			4'h7: display = 7'b0001111;
-			4'h8: display = 7'b0000000;
-			4'h9: display = 7'b0001100;
-			default: display = 7'b0000000;
-		endcase
 endmodule
