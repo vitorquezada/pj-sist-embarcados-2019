d3c476af4b39d727947abfac1631ba23be48990c
Vitor Quezada <vitorquezada@gmail.com>
2019-06-26 20:33:34 -0300

    Ajuste com adição do código de MLP no módulo principal

--- detectorDeDiabetes/detectorDeDiabetes.v
+++ detectorDeDiabetes/detectorDeDiabetes.v
@@ -15,14 +15,14 @@ module detectorDeDiabetes(SW, KEY, ADC_CLK_10, HEX5, HEX4, HEX3, HEX2, HEX1, HEX
 	
 	parameter QtdBitsDecimais = 16; // Q16.16
 	parameter N = 6,
-			  Pregnancies = 0,
-			  Glucose = 1,
-			  BloodPressure = 2,
-			  SkinThickness = 3,
-			  Insulin = 4,
-			  BMI = 5,
-			  Age = 6,
-			  Outcome = 7;
+			 // Pregnancies = 0,
+			  Glucose = 0,
+			  BloodPressure = 1,
+			  SkinThickness = 2,
+			  Insulin = 3,
+			  BMI = 4,
+			  Age = 5,
+			  Outcome = 6;
 
 	reg [4:0] estado;
 	reg [4:0] resultado;
@@ -73,12 +73,71 @@ module detectorDeDiabetes(SW, KEY, ADC_CLK_10, HEX5, HEX4, HEX3, HEX2, HEX1, HEX
 		end
 	endtask
 	
+	//########################################################################################
 	task mlp;
-		
+
+		parameter signed [3:0] NEURONIOS = 4'b0111; /* 7 neuronios camada intermediaria */
+		reg signed [4:0] i;
+		reg signed [32:0] f[0:N];
+		reg signed [32:0] y;
+		reg signed [15:0] w [0:N][0:NEURONIOS]; /* camada intermediaria */
+		reg signed [15:0] v [0:NEURONIOS]; /* camada saida */
+
+		parameter signed [15:0] zero= 16'b00000000_00000000;
+		parameter signed [15:0] um= 16'b00000001_00000000;
+
 		begin
-		
-		end	
+
+			i = 4'b0000;
+			
+			/* CAMADA INTERMEDIARIA */
+			for (i = 0; i < (NEURONIOS - 1); i = i + 1) begin
+			w[0][i] = 16'b00000000_00010001;			
+			w[1][i] = 16'b00000000_00010001;			
+			w[2][i] = 16'b00000000_00010001;
+			w[3][i] = 16'b00000000_00010001; 
+			w[4][i] = 16'b00000000_00010001;
+			w[5][i] = 16'b00000000_00010001;
+			w[6][i] = 16'b00000000_00010001;
+			v[i] = 16'b00000000_00010001;
+			/* Soma */
+			f[i]  =   entradas[0]*w[0][i] +
+				entradas[1]*w[1][i] +
+				entradas[2]*w[2][i] +
+				entradas[3]*w[3][i] +
+				entradas[4]*w[4][i] +
+				entradas[5]*w[5][i]+
+				w[6][i]; //bias
+
+			/* Relu com limite superior*/
+			if(f[i] > um)
+			f[i] = 16'b00000001_00000000;//um;
+			else if(f[i] < zero)
+			f[i] = 16'b00000000_00000000;//zero;
+				
+			end
+
+			/* CAMADA SAIDA */
+			y = f[0][23:8]*v[0]+
+			f[1][23:8]*v[1]+
+			f[2][23:8]*v[2]+
+			f[3][23:8]*v[3]+
+			f[4][23:8]*v[4]+
+			f[5][23:8]*v[5]+
+			f[6][23:8]*v[6]+
+			v[7];
+
+			if(     y > 32'b0000000000000000_1001100110011001)      //y > 0.6
+			resultado = 4;              //y = 3;
+			else if(y > 32'b0000000000000000_1000000000000000) // 0.6 > y > 0.5
+			resultado = 5;              //y = 2;
+			else if(y > 32'b0000000000000000_0110011001100110) // 0.5 > y > 0.4
+			resultado = 6;              //y = 1;
+			else                        //y < 0.4
+			resultado = 2;              //y = 0;
+		end
 	endtask
+	//########################################################################################
 	
 	task taskClear;
 		begin
@@ -125,16 +184,16 @@ module detectorDeDiabetes(SW, KEY, ADC_CLK_10, HEX5, HEX4, HEX3, HEX2, HEX1, HEX
 			else
 			begin
 				estado = estado + 1;
-				if(estado == Outcome)
+				if(estado == 6)
 				begin
 					for(i = 0; i < N; i = i + 1)
 					begin
 						entradas[i] = entradas[i] << QtdBitsDecimais;
 					end
 
-					mlp();
+					mlp(); //########################################################
 
-					numeroVetorizado[estado][3] = 0;
+					numeroVetorizado[estado][3] = 8;
 					numeroVetorizado[estado][2] = 0;
 					numeroVetorizado[estado][1] = 0;
 					numeroVetorizado[estado][0] = resultado;
